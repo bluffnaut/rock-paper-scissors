446e0c37def2fe7a10ead4b206767d8000122663
bluffnaut <149443845+bluffnaut@users.noreply.github.com>
2025-03-22 16:21:24 -0600

    Delete conditional logic that wasn't working
    Deleted the if else statements since I need to review the syntax

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,6 +1,7 @@
 // Function to get the computer's choice for Rock Paper Scissors
+// Expected output: 0, 1 or 2
 function getComputerChoice(max = 3) {
-    let randomNum = Math.floor(Math.random() * 3); // Expected output: 0, 1 or 2
+    let randomNum = Math.floor(Math.random() * 3);
 
 switch (randomNum) {
 case 0:
@@ -9,14 +10,14 @@ case 1:
    return "Paper";
 case 2: 
    return "Scissors";
-} // Expected output: Rock, Paper or Scissors
-
+}
 }
 
 
 // Variable to store the computer's choice
-let computerChoice = getComputerChoice(3); // Runs the getComputerChoice function
-console.log(computerChoice); // Expected output: Rock, Paper or Scissors
+// Expected output: Rock, Paper or Scissors
+let computerChoice = getComputerChoice(3);
+console.log(computerChoice);
 
 
 // Function to get the user's choice for Rock Paper Scissors
@@ -27,14 +28,11 @@ function getHumanChoice() {
 
 
 // Variable to store the user's choice
-const humanChoice = getHumanChoice();
+let humanChoice = getHumanChoice();
 console.log(humanChoice);
 
 
 // Variables to store the score of each player
 let humanScore = 0
-let computer Score = 0
+let computerScore = 0
 
-
-// Function to play a single round of Rock Paper Scissors
-function playRound(humanChoice, computerChoice);
