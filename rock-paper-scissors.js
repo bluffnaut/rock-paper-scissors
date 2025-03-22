c223f8e5159339af43a6968f481ed7c9c56f9106
bluffnaut <149443845+bluffnaut@users.noreply.github.com>
2025-03-22 14:03:46 -0600

    Create a variable to store the computer's choice
    Added the variable computerChoice, along with more comments for context

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -13,7 +13,10 @@ case 2:
 
 }
 
-console.log(getComputerChoice(3));
+
+// Variable to store the computer's choice
+let computerChoice = getComputerChoice(3); // Runs the getComputerChoice function
+console.log(computerChoice); // Expected output: Rock, Paper or Scissors
 
 
 // Function to get the user's choice for Rock Paper Scissors
@@ -22,5 +25,16 @@ function getHumanChoice() {
    return input;
 }
 
+
+// Variable to store the user's choice
 const humanChoice = getHumanChoice();
 console.log(humanChoice);
+
+
+// Variables to store the score of each player
+let humanScore = 0
+let computer Score = 0
+
+
+// Function to play a single round of Rock Paper Scissors
+function playRound(humanChoice, computerChoice);
