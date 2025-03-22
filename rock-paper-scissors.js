0556b6846ae8e95fb2d8a63ee43179dda106d868
bluffnaut <149443845+bluffnaut@users.noreply.github.com>
2025-03-22 13:38:04 -0600

    Write code to get the computer's choice
    The computer's choice will be logged for Rock Paper Scissors

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,1 +1,15 @@
-console.log("Hello, World!")
+function getComputerChoice(max = 3) {
+    let randomNum = Math.floor(Math.random() * 3); // Expected output: 0, 1 or 2
+
+switch (randomNum) {
+case 0:
+   return "Rock"; 
+case 1: 
+   return "Paper";
+case 2: 
+   return "Scissors";
+} // Expected output: Rock, Paper or Scissors
+
+}
+
+console.log(getComputerChoice(3));
