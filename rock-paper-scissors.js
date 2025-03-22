fc749b4f906b7aa9d4c7199869983d9c3bf2ded3
bluffnaut <149443845+bluffnaut@users.noreply.github.com>
2025-03-22 13:43:52 -0600

    Create code to log the user's input
    Created code to log the user's input for Rock Paper Scissors

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,3 +1,4 @@
+// Function to get the computer's choice for Rock Paper Scissors
 function getComputerChoice(max = 3) {
     let randomNum = Math.floor(Math.random() * 3); // Expected output: 0, 1 or 2
 
@@ -13,3 +14,13 @@ case 2:
 }
 
 console.log(getComputerChoice(3));
+
+
+// Function to get the user's choice for Rock Paper Scissors
+function getHumanChoice() {
+   let input = window.prompt("Type Rock, Paper, or Scissors", ""); // Prompts the user to submit Rock, Paper, or Scissors
+   return input;
+}
+
+const humanChoice = getHumanChoice();
+console.log(humanChoice);
