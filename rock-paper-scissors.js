73a5620c23589b69dc283ee55cc74dc49b3e85ad
bluffnaut <149443845+bluffnaut@users.noreply.github.com>
2025-03-24 18:59:54 -0600

    Reorganize the Javascript code and add comments
    Added more comments to make the code easier to read

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,15 +1,20 @@
+// Variables to store the current scores
+let humanScore = 0
+let computerScore = 0
+
+
 // Function to get the computer's choice for Rock Paper Scissors
 // Expected output: 0, 1 or 2
 function getComputerChoice(max = 3) {
-    let randomNum = Math.floor(Math.random() * 3);
+   let randomNum = Math.floor(Math.random() * 3);
 
 switch (randomNum) {
 case 0:
-   return "Rock"; 
+  return "Rock"; 
 case 1: 
-   return "Paper";
+  return "Paper";
 case 2: 
-   return "Scissors";
+  return "Scissors";
 }
 }
 
@@ -18,48 +23,12 @@ case 2:
 let computerChoice = getComputerChoice(3);
 
 
-// Function to determine the winner of a round
-function playRound(humanChoice, computerChoice) {
-   
-   if (humanChoice === computerChoice) {
-      return "Tie!"
-   }
-   
-   if (humanChoice === "Rock") {
-    if (computerChoice === "Paper") {
-      computerScore++;
-      return "You lose. Paper beats rock.";
-   } else {
-      humanScore++;
-      return "You win! Rock beats scissors.";
-   }
-   }
+// Function to log the computer's choice
+// Expected output: Rock, Paper or Scissors
+console.log(computerChoice);
 
 
-   if (humanChoice === "Paper") {
-    if (computerChoice === "Scissors") {
-      computerScore++;
-      return "You lose. Scissors beats paper.";
-   } else {
-      humanScore++;
-      return "You win! Paper beats rock.";
-   }
-   }
-
-   
-   if (humanChoice === "Scissors") {
-    if (computerChoice === "Rock") {
-      computerScore++;
-      return "You lose. Rock beats scissors.";
-   } else {
-      humanScore++;
-      return "You win! Scissors beats paper.";
-   }
-   }
-}
-
-
-// Function to get the user's choice for Rock Paper Scissors
+// Function to get the user's choice
 function getHumanChoice() {
    let input = window.prompt("Type Rock, Paper, or Scissors", ""); // Prompts the user to submit Rock, Paper, or Scissors
    return input;
@@ -70,16 +39,54 @@ function getHumanChoice() {
 let humanChoice = getHumanChoice();
 
 
-// Variables to store the current scores
-let humanScore = 0
-let computerScore = 0
-
-
-// Log the computer's choice
-// Expected output: Rock, Paper or Scissors
-console.log(computerChoice);
-
-// Log the player's choice
+// Function to log the player's choice
 console.log(humanChoice);
 
+
+// Function to start a round of Rock Paper Scissors
 console.log(playRound(humanChoice, computerChoice));
+
+
+// Function to determine the winner of a round
+function playRound(humanChoice, computerChoice) {
+   if (humanChoice === computerChoice) {
+      console.log("Tie!");
+      return("You " + humanScore + " Computer " + computerScore);
+   }
+   
+   if (humanChoice === "Rock") {
+    if (computerChoice === "Paper") {
+      computerScore++;
+      console.log("You lose. Paper beats rock.");
+      console.log("You " + humanScore, "Computer " + computerScore);
+   } else {
+      humanScore++;
+      console.log("You win! Rock beats scissors.");
+      console.log("You " + humanScore, "Computer " + computerScore);
+   }
+   }
+
+   if (humanChoice === "Paper") {
+    if (computerChoice === "Scissors") {
+      computerScore++;
+      console.log("You lose. Scissors beats paper.");
+      console.log("You " + humanScore, "Computer " + computerScore);
+   } else {
+      humanScore++;
+      console.log("You win! Paper beats rock.");
+      console.log("You " + humanScore, "Computer " + computerScore);
+   }
+   }
+
+   if (humanChoice === "Scissors") {
+    if (computerChoice === "Rock") {
+      computerScore++;
+      console.log("You lose. Rock beats scissors.");
+      console.log("You " + humanScore, "Computer " + computerScore);
+   } else {
+      humanScore++;
+      console.log("You win! Scissors beats paper.");
+      console.log("You " + humanScore, "Computer " + computerScore);
+   }
+   }
+}
