a8e53dfd3f99a346982bfc6e8577820a32d806d2
bluffnaut <149443845+bluffnaut@users.noreply.github.com>
2025-10-05 16:02:23 -0600

    Clean the JavaScript code
    Prepares for the new UI with cleaner JavaScript

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,121 +1,78 @@
-/* This comment deactivates all Javascript
+const choices = ["rock", "paper", "scissors"];
+const winners = [];
 
-// Variables to store the current scores
-let humanScore = 0
-let computerScore = 0
-
-
-// Variable to store the computer's choice
-let computerChoice = getComputerChoice(3);
-
-
-// Variable to store the player's choice
-   let humanChoice = getHumanChoice();
-
-
-// Function to get the computer's choice for Rock Paper Scissors
-// Expected output: 0, 1 or 2
-function getComputerChoice(max = 3) {
-   let randomNum = Math.floor(Math.random() * 3);
-
-switch (randomNum) {
-case 0:
-  return "Rock"; 
-case 1: 
-  return "Paper";
-case 2: 
-  return "Scissors";
-}
+function game() {
+   for (let i = 1; i < 6; i++) {
+      playRound(i);
+   }
+   // document.querySelector('button').textContent = "Play new game"
+   logWins();
 }
 
+function playRound(round) {
+   const playerSelection = playerChoice();
+   const computerSelection = computerChoice();
+   const winner = checkWinner(playerSelection, computerSelection);
+   winners.push(winner);
+   logRound(playerSelection, computerSelection, winner, round)
+}
 
-// Function to log the computer's choice
-// Expected output: Rock, Paper or Scissors
-console.log(computerChoice);
-
-
-// Function to get the user's choice
-function getHumanChoice() {
-   let input = window.prompt("Type Rock, Paper, or Scissors", ""); // Prompts the user to submit Rock, Paper, or Scissors
+function playerChoice() {
+   let input = prompt('Type Rock, Paper, or Scissors');
+   while (input == null) {
+      input = prompt('Type Rock, Paper, or Scissors');
+   }
+   input = input.toLowerCase();
+   let check = validateInput(input);
+   while (check == false) {
+      input = prompt(
+         'Type Rock, Paper, or Scissors. Spelling needs to be exact.'
+      );
+      while (input == null) {
+      input = prompt('Type Rock, Paper, or Scissors');
+      }
+      input = input.toLowerCase()
+      check = validateInput(input);
+   }
    return input;
 }
 
+function computerChoice() {
+   return choices[Math.floor(Math.random() * choices.length)];
+}
 
-// Function to log the player's choice
-console.log(humanChoice);
+function validateInput(choice) {
+   return choices.includes(choice);
+}
 
-
-// Function to start a round of Rock Paper Scissors
-console.log(playRound(humanChoice, computerChoice));
-
-
-// Function to determine the winner of a round
-function playRound(humanChoice, computerChoice) {
-   if (humanChoice === computerChoice) {
-      console.log("Tie!");
-      console.log("You " + humanScore + " Computer " + computerScore);
-      return;
-   }
-   
-   if (humanChoice === "Rock") {
-    if (computerChoice === "Paper" && computerChoice !== "Rock") {
-      computerScore++;
-      console.log("You lose. Paper beats rock.");
-      console.log("You " + humanScore, "Computer " + computerScore);
+function checkWinner(choiceP, choiceC) {
+   if (choiceP === choiceC) {
+      return "Tie";
+   } else if (
+      (choiceP === "rock" && choiceC === "scissors") || 
+      (choiceP === "paper" && choiceC === "rock") || 
+      (choiceP === "scissors" && choiceC === "paper")
+   ) {
+      return 'Player';
    } else {
-      humanScore++;
-      console.log("You win! Rock beats scissors.");
-      console.log("You " + humanScore, "Computer " + computerScore);
-   }
-   }
-
-   if (humanChoice === "Paper") {
-    if (computerChoice === "Scissors") {
-      computerScore++;
-      console.log("You lose. Scissors beats paper.");
-      console.log("You " + humanScore, "Computer " + computerScore);
-   } else {
-      humanScore++;
-      console.log("You win! Paper beats rock.");
-      console.log("You " + humanScore, "Computer " + computerScore);
-   }
-   }
-
-   if (humanChoice === "Scissors") {
-    if (computerChoice === "Rock") {
-      computerScore++;
-      console.log("You lose. Rock beats scissors.");
-      console.log("You " + humanScore, "Computer " + computerScore);
-   } else {
-      humanScore++;
-      console.log("You win! Scissors beats paper.");
-      console.log("You " + humanScore, "Computer " + computerScore);
-   }
+      return 'Computer';
    }
 }
 
-
-// Function to play the game until a player reaches 3 points
-function playGame() {
-   if (humanScore === 3) {
-       console.log("Congratulations! You won the game!");
-       return;
-   }
-   if (computerScore === 3) {
-       console.log("Computer won the game! Better luck next time.");
-       return;
-   }
-
-// Variable to store the computer's choice
-   let computerChoice = getComputerChoice(3);
-
-
-// Variable to store the player's choice
-   let humanChoice = getHumanChoice();
-   playRound(humanChoice, computerChoice);
-
-   playGame(); // Recursively call playGame until someone wins
+function logWins() {
+   let playerWins = winners.filter((item) => item == 'Player').length;
+   let computerWins = winners.filter((item) => item == 'Computer').length;
+   let ties = winners.filter((item) => item == 'Tie').length;
+   console.log('Results:');
+   console.log('Player Wins:', playerWins);
+   console.log('Computer Wins:', computerWins);
+   console.log('Ties', ties);
 }
 
-// Start the game
-playGame();
+function logRound(playerChoice, computerChoice, winner, round) {
+   console.log('Round:', round);
+   console.log('Player Chose:', playerChoice);
+   console.log('Computer Chose:', computerChoice);
+   console.log(winner, 'Won the Round');
+   console.log("--------------------");
+}
